b1c65e8584bfea9e37297170f229af1bfbb8e54a
Kauan Da Silva <kauan@sunfieldmarketing.com>
2026-05-13 04:43:40 -0400

    fix: add mandatory tina-lock.json for branch authorization

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/TEGAC/.gemini/antigravity/scratch/evolve-therapy-services/tina/__generated__/.cache/1778661647957', url: 'http://localhost:4001/graphql', token: '5b6b1935560b9b806fa1ceb712713010e0e32bab', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '5b6b1935560b9b806fa1ceb712713010e0e32bab', queries,  });
 export default client;
   
